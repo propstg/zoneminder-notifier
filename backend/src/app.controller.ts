b0547cb4e8ec69b54694f3f43b947d49b81ac7d2
propstg <g@prop.st>
2020-12-21 22:44:34 -0500

    remove removed class

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,12 @@
 import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { CameraDefinitionService } from './cameraDefinitions/camera-definition.service';
 import { CameraDefinition } from './config/cameraDefinition';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private readonly cameraDefinitionService: CameraDefinitionService
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @Get("/camera/:id")
   getNotificationStatus(@Param("id") cameraId: string) {
